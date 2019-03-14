acb4fd37aa58cfc6d352218836c4e81e9fb1efc8
Rafael Costa <rafael@rafaelcosta.me>
2019-03-14 11:52:44 -0300

    Initial commit

---
+++ Double+Tau.swift
@@ -0,0 +1,9 @@
+/*
+** FREE YOURSELF FROM THE PI OPPRESSION.
+** READ MORE AT https://tauday.com/tau-manifesto
+** CREATED BY RAFAEL COSTA (rafael@rafaelcosta.me or twitter.com/merafaelcosta), PROVIDED AS IS. 
+*/
+
+extension Double {
+    static let tau = Double.pi * 2
+}
